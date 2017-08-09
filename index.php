5eef5a5fb39e9dc5a50c4def45d5769e34153d61
stephen rowan <rowans55@yahoo.co.uk>
2017-08-09 15:44:23 +0000

    Colour and layout changes to index.php

--- index.php
+++ index.php
@@ -1,43 +1,28 @@
 <?php echo head(array('bodyid'=>'home')); ?>
 
 
-<?php if (get_theme_option('Display Featured Item') !== '0'): ?>
-
-<!-- Featured Item -->
-<div id="featured-item">
-<p>
-
+<div id="home-page-text">
 <?php if (get_theme_option('Homepage Text')): ?>
-<?php echo get_theme_option('Homepage Text'); ?>
+<p><?php echo get_theme_option('Homepage Text'); ?></p>
 <?php endif; ?>
-</p>
-
-</div><!--end featured-item-->
-<?php endif; ?>
-
-
-
-<?php fire_plugin_hook('public_content_top', array('view'=>$this)); ?>
-
+</div>
 
 <!-- Browse our collections -->
-<div id="featured-collection">
+<div id="browse-our-collections">
     <h2>Browse our collections</h2>
-    
-    
-   
-   
+
 </div><!-- end Browse our collections -->
 
 
 <?php if (get_theme_option('Display Featured Item') !== '0'): ?>
 <!-- Featured Item -->
-<div id="featured-collection">
-    <h2><?php echo __('Featured items'); ?></h2>
-   <center><?php echo random_featured_items(2); ?></center>
-   
+<div id="featured-item">
+    <h2><?php echo __('Featured Item'); ?></h2>
+    <?php echo random_featured_items(1); ?>
 </div><!--end featured-item-->
 <?php endif; ?>
+
+<?php //fire_plugin_hook('public_content_top', array('view'=>$this)); ?>
 
 
 <?php if (get_theme_option('Display Featured Collection') !== '0'): ?>
@@ -52,10 +37,9 @@
         && plugin_is_active('ExhibitBuilder')
         && function_exists('exhibit_builder_display_random_featured_exhibit')): ?>
 <!-- Featured Exhibit -->
+<div id="featured-exhibit">
 <?php echo exhibit_builder_display_random_featured_exhibit(); ?>
 <?php endif; ?>
-
-
-<?php fire_plugin_hook('public_home', array('view' => $this)); ?>
+</div>
 
 <?php echo foot(); ?>
