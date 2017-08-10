7d37a8c673e6e1e2e992091db317c4559724aea6
stephen rowan <rowans55@yahoo.co.uk>
2017-08-10 15:08:41 +0000

    Applying hex codes to Collection formats

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 
 <div id="home-page-text">
 <?php if (get_theme_option('Homepage Text')): ?>
-<p><?php echo get_theme_option('Homepage Text'); ?></p>
+<h3><?php echo get_theme_option('Homepage Text'); ?></h3>
 <?php endif; ?>
 </div>
 
 <!-- Browse our collections -->
 <div id="browse-our-collections">
-    <h2>Browse our collections</h2>
+<h3><a href="/collections/browse/">Browse our collections</a></h3>
 
 </div><!-- end Browse our collections -->
 
@@ -17,7 +17,7 @@
 <?php if (get_theme_option('Display Featured Item') !== '0'): ?>
 <!-- Featured Item -->
 <div id="featured-item">
-    <h2><?php echo __('Featured Item'); ?></h2>
+    <h3><?php echo __('Featured Item'); ?></h3>
     <?php echo random_featured_items(1); ?>
 </div><!--end featured-item-->
 <?php endif; ?>
@@ -28,7 +28,7 @@
 <?php if (get_theme_option('Display Featured Collection') !== '0'): ?>
 <!-- Featured Collection -->
 <div id="featured-collection">
-    <h2><?php echo __('Featured Collection'); ?></h2>
+    <h3><?php echo __('Featured Collection'); ?></h3>
     <?php echo random_featured_collection(); ?>
 </div><!-- end featured collection -->
 <?php endif; ?>
@@ -38,8 +38,13 @@
         && function_exists('exhibit_builder_display_random_featured_exhibit')): ?>
 <!-- Featured Exhibit -->
 <div id="featured-exhibit">
-<?php echo exhibit_builder_display_random_featured_exhibit(); ?>
+<h3><?php echo exhibit_builder_display_random_featured_exhibit(); ?></h3>
 <?php endif; ?>
 </div>
 
 <?php echo foot(); ?>
+
+
+
+
+
